eabe887bc7c100665e67a846e8425725c2935532
Meefish <117977321+Meefish@users.noreply.github.com>
2025-01-08 23:03:41 +0100

    Fixes #61 Fixes #62

--- src/services/messageService.ts
+++ src/services/messageService.ts
@@ -1,6 +1,6 @@
 import { Message } from '../types/message';
 const MESSAGE_API_URL = 'https://wildlifenl-uu-michi011.apps.cl01.cp.its.uu.nl/message/';
-const MESSAGES_API_URL = 'https://wildlifenl-uu-michi011.apps.cl01.cp.its.uu.nl/messages/';
+const MESSAGES_API_URL = 'https://wildlifenl-uu-michi011.apps.cl01.cp.its.uu.nl/messages/experiment/';
 
 const getAuthToken = (): string | null => {
   return localStorage.getItem('authToken');
--- src/services/questionnaireService.ts
+++ src/services/questionnaireService.ts
@@ -1,6 +1,6 @@
 import {AddQuestionnaire, Questionnaire} from '../types/questionnaire'
 const QUESTIONNAIRE_API_URL = 'https://wildlifenl-uu-michi011.apps.cl01.cp.its.uu.nl/questionnaire/';
-const QUESTIONNAIRES_API_URL = 'https://wildlifenl-uu-michi011.apps.cl01.cp.its.uu.nl/questionnaires/';
+const QUESTIONNAIRES_API_URL = 'https://wildlifenl-uu-michi011.apps.cl01.cp.its.uu.nl/questionnaires/experiment/';
 
 
 const getAuthToken = (): string | null => {
