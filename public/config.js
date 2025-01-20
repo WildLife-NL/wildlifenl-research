254fb8c6090e3d70e646db4e3f1deaf802ebf05a
Meefish <117977321+Meefish@users.noreply.github.com>
2025-01-20 15:47:38 +0100

    OpenShift Solution 1st Try

---
+++ public/config.js
@@ -0,0 +1,4 @@
+// public/config.js
+window.ENV = {
+  BASE_URL: "http://localhost:3000"  // fallback for local dev
+};
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,9 +1,10 @@
 import { getMyProfile } from './profileService';
+import { getBaseUrl } from '../config';
 
 const TEST_BASE_URL = 'https://wildlifenl-uu-michi011.apps.cl01.cp.its.uu.nl';
 const BASE_URL = process.env.NODE_ENV === 'test'
   ? TEST_BASE_URL
-  : process.env.REACT_APP_BASE_URL;
+  : getBaseUrl();
 
 const API_URL = `${BASE_URL}/auth/`;
 
